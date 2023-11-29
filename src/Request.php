a682fc89e8ab318b0218d05107e228140b97465d
halillusion <halillusion@gmail.com>
2023-11-29 17:48:47 +0300

    Refactor Request class constructor and methods

--- src/Request.php
+++ src/Request.php
@@ -15,112 +15,112 @@ use KX\Core\Exception;
 final class Request
 {
 
-  private $request;
-  private $requestUri;
-  private $requestMethod;
-  private $queryString;
-  private $getParams;
-  private $postParams;
-  private $header;
+    private $request;
+    private $requestUri;
+    private $requestMethod;
+    private $queryString;
+    private $getParams;
+    private $postParams;
+    private $header;
 
 
 
-  /**
-   * Request constructor
-   * @return object
-   */
-  public function __construct()
-  {
+    /**
+     * Request constructor
+     * @return object
+     */
+    public function __construct()
+    {
 
-    $this->request = $_REQUEST;
-    $this->requestUri = $_SERVER['REQUEST_URI'];
-    $this->requestMethod = $_SERVER['REQUEST_METHOD'];
-    $this->queryString = $_SERVER['QUERY_STRING'];
-    $this->header = getallheaders();
-    $this->getParams = $_GET;
-    $this->postParams = $_POST;
+        $this->request = $_REQUEST;
+        $this->requestUri = $_SERVER['REQUEST_URI'];
+        $this->requestMethod = $_SERVER['REQUEST_METHOD'];
+        $this->queryString = $_SERVER['QUERY_STRING'];
+        $this->header = getallheaders();
+        $this->getParams = $_GET;
+        $this->postParams = $_POST;
 
-    return $this;
-  }
+        return $this;
+    }
 
-  /**
-   * Get request method
-   * @return string
-   */
-  public function getRequestMethod(): string
-  {
-    return $this->requestMethod;
-  }
+    /**
+     * Get request method
+     * @return string
+     */
+    public function getRequestMethod(): string
+    {
+        return $this->requestMethod;
+    }
 
-  /**
-   * Get request uri
-   * @return string
-   */
-  public function getUri(): string
-  {
-    return $this->requestUri;
-  }
+    /**
+     * Get request uri
+     * @return string
+     */
+    public function getUri(): string
+    {
+        return $this->requestUri;
+    }
 
-  /**
-   * Get query string
-   * @return string
-   */
-  public function getQueryString(): string
-  {
-    return $this->queryString;
-  }
+    /**
+     * Get query string
+     * @return string
+     */
+    public function getQueryString(): string
+    {
+        return $this->queryString;
+    }
 
-  /**
-   * Get request header
-   * @return array
-   */
-  public function getHeader(): array
-  {
-    return $this->header;
-  }
+    /**
+     * Get request header
+     * @return array
+     */
+    public function getHeader(): array
+    {
+        return $this->header;
+    }
 
-  /**
-   * Get request params
-   * @return array
-   */
-  public function getRequest(): array
-  {
-    return $this->request;
-  }
+    /**
+     * Get request params
+     * @return array
+     */
+    public function getRequest(): array
+    {
+        return $this->request;
+    }
 
-  /**
-   * Get request params
-   * @return array
-   */
-  public function getGetParams(): array
-  {
-    return $this->getParams;
-  }
+    /**
+     * Get request params
+     * @return array
+     */
+    public function getGetParams(): array
+    {
+        return $this->getParams;
+    }
 
-  /**
-   * Get request params
-   * @return array
-   */
-  public function getPostParams(): array
-  {
-    return $this->postParams;
-  }
+    /**
+     * Get request params
+     * @return array
+     */
+    public function getPostParams(): array
+    {
+        return $this->postParams;
+    }
 
-  /**
-   * Get request params
-   * @return array
-   */
-  public function getParams(): array
-  {
-    return array_merge($this->getParams, $this->postParams);
-  }
+    /**
+     * Get request params
+     * @return array
+     */
+    public function getParams(): array
+    {
+        return array_merge($this->getParams, $this->postParams);
+    }
 
-  /**
-   * Get request params
-   * @return array
-   */
-  public function getParam(string $key): string
-  {
-    return $this->getParams[$key] ?? $this->postParams[$key] ?? '';
-  }
+    /**
+     * Get request params
+     * @return array
+     */
+    public function getParam(string $key): string
+    {
+        return $this->getParams[$key] ?? $this->postParams[$key] ?? '';
+    }
 }
