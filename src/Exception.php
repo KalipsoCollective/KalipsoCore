2c8c61575b00b58a16a2812c1a9587851326e7a1
halillusion <halillusion@gmail.com>
2023-11-29 17:48:25 +0300

    Refactor error handling and add custom error handler

--- src/Exception.php
+++ src/Exception.php
@@ -9,8 +9,6 @@ declare(strict_types=1);
 
 namespace KX\Core;
 
-use KX\Core\Helper;
-
 /**
  * Exception class
  *
@@ -18,55 +16,92 @@ use KX\Core\Helper;
  * @subpackage Core\Exception
  */
 
+use KX\Core\Request;
+use KX\Core\Response;
+
 final class Exception
 {
 
-	/**
-	 *  Fatal error handler
-	 **/
-	public static function fatalHandler()
-	{
+    private static $customErrorHandler = null;
 
-		$error = error_get_last();
-		if (!is_null($error) and is_array($error) and $error["type"] == E_ERROR) {
-			self::errorHandler($error["type"], $error["message"], $error["file"], $error["line"]);
-		}
-	}
+    private static $possibleHttpStatusCodes = [
+        401 => 'Unauthorized',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        405 => 'Method Not Allowed',
+        503 => 'Service Unavailable'
+    ];
 
-	/**
-	 *  Error handler output
-	 * @param int $errNo
-	 * @param string $errMsg
-	 * @param string $file
-	 * @param int $line
-	 * @return void
-	 **/
-	public static function errorHandler($errNo, string $errMsg, string $file, int $line)
-	{
+    /**
+     *  Fatal error handler
+     **/
+    public static function fatalHandler()
+    {
 
-		ob_get_clean();
-		ob_start();
+        $error = error_get_last();
+        if (!is_null($error) and is_array($error) and $error["type"] == E_ERROR) {
+            self::errorHandler($error["type"], $error["message"], $error["file"], $error["line"]);
+        }
+    }
 
-		$output = '';
-		if (Helper::config('DEV_MODE', true)) {
-			$output .= $file . ':' . $line . ' - ';
-		}
-		$output .= $errMsg;
-		if ($errNo) {
-			$output .= ' <strong>(' . $errNo . ')</strong>';
-		}
+    /**
+     *  Error handler output
+     * @param int $errNo
+     * @param string $errMsg
+     * @param string $file
+     * @param int $line
+     * @return void
+     **/
+    public static function errorHandler($errNo, string $errMsg, string $file, int $line, $context = null)
+    {
 
-		if (isset($_SERVER['HTTP_ACCEPT']) !== false and $_SERVER['HTTP_ACCEPT'] === 'application/json') {
+        ob_get_clean();
+        ob_start();
 
-			Helper::http('content_type', [
-				'content' => 'json',
-				'write' => json_encode([
-					'error' => htmlspecialchars($output)
-				])
-			]);
-		} else {
+        if (
+            is_array(self::$customErrorHandler) &&
+            count(self::$customErrorHandler) === 3 &&
+            is_callable(self::$customErrorHandler[2])
+        ) {
 
-			$handlerOutput = '
+            call_user_func_array(
+                self::$customErrorHandler[2],
+                [
+                    self::$customErrorHandler[0],
+                    self::$customErrorHandler[1],
+                    $errNo,
+                    $errMsg,
+                    $file,
+                    $line,
+                    $context
+                ]
+            );
+            exit;
+        }
+
+        $output = '';
+        if (Helper::config('DEV_MODE', true)) {
+            $output .= $file . ':' . $line . ' - ';
+        }
+        $output .= $errMsg;
+        if ($errNo) {
+            $output .= ' <strong>(' . $errNo . ')</strong>';
+        }
+        // set http status code
+        if (isset(self::$possibleHttpStatusCodes[$errNo]) !== false) {
+            header($_SERVER['SERVER_PROTOCOL'] . ' ' . $errNo . ' ' . self::$possibleHttpStatusCodes[$errNo], true, $errNo);
+        } else {
+            header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
+        }
+
+        if (isset($_SERVER['HTTP_ACCEPT']) !== false and $_SERVER['HTTP_ACCEPT'] === 'application/json') {
+            header('Content-Type: application/json, charset=utf-8');
+            echo json_encode([
+                'error' => htmlspecialchars($output)
+            ]);
+        } else {
+            header('Content-Type: text/html, charset=utf-8');
+            $handlerOutput = '
 			<!doctype html>
 			<html>
 				<head>
@@ -99,24 +134,39 @@ final class Exception
 				</body>
 			</html>';
 
-			$errorOutput = '    ' . $output;
-			echo str_replace('[OUTPUT]', $errorOutput, $handlerOutput);
-		}
-		exit;
-	}
+            $errorOutput = '    ' . $output;
+            echo str_replace('[OUTPUT]', $errorOutput, $handlerOutput);
+        }
+        exit;
+    }
 
-	/**
-	 *  Exception handler
-	 **/
-	static function exceptionHandler($e = null)
-	{
+    /**
+     *  Exception handler
+     **/
+    static function exceptionHandler($e = null)
+    {
 
-		if (is_null($e)) {
+        if (is_null($e)) {
 
-			die('Not handledable.');
-		} else {
+            die('Not handledable.');
+        } else {
 
-			self::errorHandler($e->getCode(), $e->getMessage(), $e->getFile(), $e->getLine());
-		}
-	}
+            self::errorHandler($e->getCode(), $e->getMessage(), $e->getFile(), $e->getLine());
+        }
+    }
+
+    /**
+     *  Custom error handler
+     **/
+    public static function setErrorHandler(Request $request, Response $response, $callback = null)
+    {
+
+        if (is_callable($callback)) {
+
+            self::$customErrorHandler = [$request, $response, $callback];
+        } else {
+
+            throw new \Exception('Custom error handler must be callable.');
+        }
+    }
 }
