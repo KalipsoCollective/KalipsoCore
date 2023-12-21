708a606b3786f7e7f3a2c1dc388147ab7e100bfb
halillusion <halillusion@gmail.com>
2023-12-21 18:01:38 +0300

    Model bulk data importer

--- src/Model.php
+++ src/Model.php
@@ -18,6 +18,7 @@ class Model extends Pdox
 
     protected $table = '';
     protected $schema;
+    protected $bulkData = [];
 
     public function __construct()
     {
@@ -88,6 +89,17 @@ class Model extends Pdox
     }
 
     /**
+     * Set bulk data
+     * @param array $data
+     * @return object
+     */
+    public function setBulkData(array $data): object
+    {
+        $this->bulkData = $data;
+        return $this;
+    }
+
+    /**
      * Set table name
      * @param string $tableName
      * @return object
@@ -100,13 +112,13 @@ class Model extends Pdox
 
     /**
      * Create table
+     * @param bool $importData
      * @return void
      */
-    public function setupModels()
+    public function setupModels($importData = false)
     {
         // delete all database tables
-        $sql = '
-        SELECT 
+        $sql = 'SELECT 
             CONCAT(`TABLE_NAME`) 
         FROM information_schema.TABLES 
         WHERE TABLE_SCHEMA = "' . Helper::config('DB_NAME') . '"';
@@ -129,6 +141,10 @@ class Model extends Pdox
 
             $model = new $modelClass();
             $model->setupModel();
+
+            if ($importData) {
+                $model->importData();
+            }
         }
     }
 
@@ -175,182 +191,53 @@ class Model extends Pdox
 
         $query = rtrim($query, PHP_EOL . ',') . PHP_EOL . ') ENGINE=' . $this->schema->engine . ' DEFAULT CHARSET=' . $this->schema->charset . ' COLLATE=' . $this->schema->collation . ';' . PHP_EOL;
 
-        Helper::dump($query, true);
-        return;
-        $sql = '';
-        foreach ($this->schema['columns'] as $column => $attributes) {
-
-            $type = '';
-
-            switch ($attributes['type']) {
-                case 'int':
-                case 'bigint':
-                case 'tinyint':
-                case 'smallint':
-                case 'mediumint':
-                    if (isset($attributes['type_values']) === false) $attributes['type_values'] = 11;
-                    $type = $attributes['type'] . '(' . $attributes['type_values'] . ')';
-                    break;
-
-                case 'float':
-                case 'decimal':
-                case 'double':
-                case 'real':
-                    if (isset($attributes['type_values']) !== false) $attributes['type_values'] = '(' . $attributes['type_values'] . ')';
-                    $type = $attributes['type'] . $attributes['type_values'];
-                    break;
-
-                case 'char':
-                case 'varchar':
-                case 'tinytext':
-                case 'text':
-                case 'mediumtext':
-                case 'longtext':
-                case 'binary':
-                case 'varbinary':
-                case 'tinyblob':
-                case 'blob':
-                case 'mediumblob':
-                case 'longblob':
-                    $type = $attributes['type'] . '(' . $attributes['type_values'] . ') COLLATE ' .
-                        (isset($attributes['collate']) !== false ? $attributes['collate'] :
-                            $this->schema['table_values']['collate']);
-
-                    break;
-
-                case 'json':
-                    $type = 'JSON ';
-
-                    break;
-
-                case 'enum':
-                case 'set':
-                    $type = $attributes['type'] . '("' . implode("', '", $attributes['type_values']) . '")';
-                    break;
-            }
-
-            if (isset($attributes['index']) !== false) {
-
-                switch ($attributes['index']) {
-                    case 'PRIMARY':
-                        $externalParams[] = 'PRIMARY KEY (`' . $column . '`)';
-                        break;
-
-                    case 'INDEX':
-                        $externalParams[] = 'INDEX `' . $column . '` (`' . $column . '`)';
-                        break;
-
-                    case 'UNIQUE':
-                        $externalParams[] = 'UNIQUE(`' . $column
-                            . '`)';
-                        break;
-
-                    case 'FULLTEXT':
-                        $externalParams[] = 'FULLTEXT(`' . $column . '`)';
-                        break;
-
-                    case 'FOREIGN':
-                        $externalParams[] = 'FOREIGN KEY (`' . $column . '`) REFERENCES `' .
-                            $attributes['foreign_table'] . '`(`' . $attributes['foreign_column'] . '`)';
-                        break;
-
-                    case 'FOREIGN_CASCADE':
-                        $externalParams[] = 'FOREIGN KEY (`' . $column . '`) REFERENCES `' .
-                            $attributes['foreign_table'] . '`(`' . $attributes['foreign_column'] . '`) ON DELETE CASCADE';
-                        break;
-
-                    case 'FOREIGN_SET_NULL':
-                        $externalParams[] = 'FOREIGN KEY (`' . $column . '`) REFERENCES `' .
-                            $attributes['foreign_table'] . '`(`' . $attributes['foreign_column'] . '`) ON DELETE SET NULL';
-                        break;
-
-                    case 'FOREIGN_RESTRICT':
-                        $externalParams[] = 'FOREIGN KEY (`' . $column . '`) REFERENCES `' .
-                            $attributes['foreign_table'] . '`(`' . $attributes['foreign_column'] . '`) ON DELETE RESTRICT';
-                        break;
-
-                    case 'FOREIGN_NO_ACTION':
-                        $externalParams[] = 'FOREIGN KEY (`' . $column . '`) REFERENCES `' .
-                            $attributes['foreign_table'] . '`(`' . $attributes['foreign_column'] . '`) ON DELETE NO ACTION';
-                        break;
-
-                    case 'FOREIGN_DEFAULT':
-                        $externalParams[] = 'FOREIGN KEY (`' . $column . '`) REFERENCES `' .
-                            $attributes['foreign_table'] . '`(`' . $attributes['foreign_column'] . '`) ON DELETE SET DEFAULT';
-                        break;
-
-                    case 'FOREIGN_CHECK':
-                        $externalParams[] = 'FOREIGN KEY (`' . $column . '`) REFERENCES `' .
-                            $attributes['foreign_table'] . '`(`' . $attributes['foreign_column'] . '`) ON DELETE CHECK';
-                        break;
-                }
-            }
-        }
-
-        if (
-            isset($attributes['attr']) !== false
-        ) {
-
-            $type .= ' ' . $attributes['attr'];
-        }
-
-        if (isset($attributes['nullable']) === false or !$attributes['nullable']) {
-            $type .= ' NOT NULL';
-        }
-
-        if (isset($attributes['default']) !== false) {
-            switch ($attributes['default']) {
-                case 'NULL':
-                case 'CURRENT_TIMESTAMP':
-                    $type .= ' DEFAULT ' . $attributes['default'];
-                    break;
-                default:
-                    $type .= ' DEFAULT \'' . $attributes['default'] . '\'';
-                    break;
-            }
-        }
-
-        if (isset($attributes['auto_inc']) !== false and $attributes['auto_inc']) {
-            $type .= ' AUTO_INCREMENT';
-        }
-
-        $sql .= PHP_EOL . '   `' . $column . '` ' . $type . ',';
-
-        if (count($externalParams)) {
-
-            foreach ($externalParams as $param) {
-                $sql .= PHP_EOL . '   ' . $param . ',';
-            }
-        }
-
-        $sql = rtrim($sql, ',' . PHP_EOL);
-
-        $engine = isset($this->schema['table_values']['specific'][$table]['engine'])  !== false ?
-            $this->schema['table_values']['specific'][$table]['engine']
-            : $this->schema['table_values']['engine'];
-
-        $charset = isset($this->schema['table_values']['specific'][$table]['charset'])  !== false ?
-
-            $this->schema['table_values']['specific'][$table]['charset']
-            : $this->schema['table_values']['charset'];
-
-        $collate = isset($this->schema['table_values']['specific'][$table]['collate'])  !== false ?
-
-            $this->schema['table_values']['specific'][$table]['collate']
-            : $this->schema['table_values']['collate'];
-
-        $sql .= PHP_EOL . ') ENGINE=' . $engine .
-            ' DEFAULT CHARSET=' . $charset .
-            ' COLLATE=' . $collate . ';' . PHP_EOL;
-
         try {
 
-            // Helper::dump($sql, true);
-            $this->pdo->exec($sql);
+            $this->pdo->exec($query);
             return true;
         } catch (\PDOException $e) {
 
-            throw new \Exception('DB Init action is not completed. ' . $e->getMessage());
+            throw new \Exception('DB migrate action is not completed. ' . $e->getMessage());
+        }
+    }
+
+    /**
+     * Import data
+     * @return void
+     */
+    public function importData()
+    {
+        if (count($this->bulkData)) {
+
+            $query = 'INSERT INTO `' . $this->table . '` (';
+
+            foreach ($this->bulkData[0] as $columnName => $columnValue) {
+                $query .= '`' . $columnName . '`,';
+            }
+
+            $query = rtrim($query, ',') . ') VALUES ';
+
+            foreach ($this->bulkData as $data) {
+
+                $query .= '(';
+
+                foreach ($data as $columnValue) {
+                    $query .= '\'' . $columnValue . '\',';
+                }
+
+                $query = rtrim($query, ',') . '),';
+            }
+
+            $query = rtrim($query, ',') . ';';
+
+            try {
+
+                $this->pdo->exec($query);
+                return true;
+            } catch (\PDOException $e) {
+
+                throw new \Exception('DB seed action is not completed. ' . $e->getMessage());
+            }
         }
     }
 }
