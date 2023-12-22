c9c013b2b9e2e957142d3485efc180fb563d3431
halillusion <halillusion@gmail.com>
2023-12-22 06:54:23 +0300

    Refactor Model class and add syncTables() method

--- src/Model.php
+++ src/Model.php
@@ -31,7 +31,7 @@ class Model extends Pdox
             'charset'   => Helper::config('DB_CHARSET'),
             'collation' => Helper::config('DB_COLLATION'),
             'prefix'    => Helper::config('DB_PREFIX'),
-            'cachedir'  => Helper::path('app/Storage/db_cache/', true)
+            'cachedir'  => Helper::path('app/Storage/db_cache/')
         ]);
 
         $this->table($this->table);
@@ -170,7 +170,15 @@ class Model extends Pdox
             }
 
             if (isset($column->default)) {
-                $query .= ' DEFAULT `' . $column->default . '`';
+                $def = '';
+                if ($column->default === 'CURRENT_TIMESTAMP') {
+                    $def = 'CURRENT_TIMESTAMP';
+                } elseif (is_null($column->default)) {
+                    $def = 'NULL';
+                } else {
+                    $def = '`' . $column->default . '`';
+                }
+                $query .= ' DEFAULT ' . $def;
             }
 
             if (isset($column->auto_increment) && $column->auto_increment) {
@@ -239,5 +247,164 @@ class Model extends Pdox
                 throw new \Exception('DB seed action is not completed. ' . $e->getMessage());
             }
         }
+    }
+
+    /**
+     * Sync tables
+     * @return object
+     */
+    public function syncTables(): object
+    {
+        // create tables
+        $modelClasses = Helper::getClasses('app/Model');
+        foreach ($modelClasses as $modelClass) {
+
+            $model = new $modelClass();
+            $model->syncTable();
+        }
+        return $this;
+    }
+
+    /**
+     * Sync table
+     * @return void
+     */
+    public function syncTable()
+    {
+        $sql = 'SELECT 
+            CONCAT(`TABLE_NAME`) 
+        FROM information_schema.TABLES 
+        WHERE TABLE_SCHEMA = "' . Helper::config('DB_NAME') . '" AND TABLE_NAME = "' . $this->table . '"';
+        $table = $this->pdo->prepare($sql);
+        $table->execute();
+        $table = $table->fetch(PDO::FETCH_COLUMN);
+
+        if (!$table) {
+
+            $this->setupModel();
+        } else {
+
+            $sql = 'SELECT 
+                CONCAT(`COLUMN_NAME`) 
+            FROM information_schema.COLUMNS 
+            WHERE TABLE_SCHEMA = "' . Helper::config('DB_NAME') . '" AND TABLE_NAME = "' . $this->table . '"';
+            $columns = $this->pdo->prepare($sql);
+            $columns->execute();
+            $columns = $columns->fetchAll(PDO::FETCH_COLUMN);
+
+            $schemaColumns = [];
+            foreach ($this->schema->columns as $columnName => $column) {
+                $schemaColumns[] = $columnName;
+            }
+
+            $diff = array_diff($schemaColumns, $columns);
+
+            if (count($diff)) {
+
+                $sql = 'ALTER TABLE `' . $this->table . '` ';
+                foreach ($diff as $columnName) {
+                    $sql .= 'ADD COLUMN `' . $columnName . '` ' . $this->schema->columns->$columnName->type . ' ';
+
+                    if (isset($this->schema->columns->$columnName->length)) {
+                        $sql .= '(' . $this->schema->columns->$columnName->length . ')';
+                    }
+
+                    if (!isset($this->schema->columns->$columnName->nullable) || !$this->schema->columns->$columnName->nullable) {
+                        $sql .= ' NOT NULL';
+                    }
+
+                    if (isset($this->schema->columns->$columnName->default)) {
+                        $def = '';
+                        if ($this->schema->columns->$columnName->default === 'CURRENT_TIMESTAMP') {
+                            $def = 'CURRENT_TIMESTAMP';
+                        } elseif (is_null($this->schema->columns->$columnName->default)) {
+                            $def = 'NULL';
+                        } else {
+                            $def = '`' . $this->schema->columns->$columnName->default . '`';
+                        }
+                        $sql .= ' DEFAULT ' . $def;
+                    }
+
+                    if (isset($this->schema->columns->$columnName->auto_increment) && $this->schema->columns->$columnName->auto_increment) {
+                        $sql .= ' AUTO_INCREMENT';
+                    }
+
+                    $sql .= ',' . PHP_EOL;
+                }
+
+                $sql = rtrim($sql, PHP_EOL . ',') . ';';
+
+                try {
+
+                    $this->pdo->exec($sql);
+                } catch (\PDOException $e) {
+
+                    throw new \Exception('DB sync action is not completed. ' . $e->getMessage());
+                }
+            }
+        }
+    }
+
+    /**
+     * insert
+     * @param array $data
+     * @param bool $type
+     * @return null|int
+     */
+    public function insert(array $data, $type = false)
+    {
+
+        if ($this->created) {
+
+            $data['created_at'] = isset($data['created_at']) === false ? time() : $data['created_at'];
+            $data['created_by'] = isset($data['created_by']) === false ? (Helper::userData('id') ?? 0) : $data['created_by'];
+        }
+
+        return parent::insert($data, $type);
+    }
+
+    /**
+     * update
+     * @param array $data
+     * @param bool $type
+     * @return null|int
+     */
+    public function update(array $data, $type = false)
+    {
+
+        if ($this->updated) {
+
+            $data['updated_at'] = isset($data['updated_at']) === false ? time() : $data['updated_at'];
+            $data['updated_by'] = isset($data['updated_by']) === false ? (Helper::userData('id') ?? 0) : $data['updated_by'];
+        }
+
+        return parent::update($data, $type);
+    }
+
+    /**
+     * reset
+     * @return void
+     */
+    protected function reset()
+    {
+
+        parent::reset();
+        $this->table($this->table);
+    }
+
+    /**
+     * cache
+     * @param $time
+     * @return object
+     */
+    public function cache($time)
+    {
+        if (Helper::config('settings.db_cache')) {
+
+            Helper::path('app/Storage/db_cache/', true);
+
+            parent::cache($time);
+        }
+        return $this;
     }
 }
