479f74e24e91c0965a04258eaa88e9481d55abf1
halillusion <halillusion@gmail.com>
2024-03-20 20:28:12 +0300

    Add logging functionality to save request and response data

--- src/Log.php
+++ src/Log.php
@@ -23,40 +23,68 @@ final class Log
      */
     public function save(Request $request, Response $response): void
     {
-        $log = [
-            'request' => [
-                'date' => date('Y-m-d H:i:s'),
-                'method' => $request->getRequestMethod(),
-                'uri' => $request->getUri(),
-                'query_string' => $request->getQueryString(),
-                'header' => $request->getHeader(),
-                'get_params' => $request->getGetParams(),
-                'post_params' => $request->getPostParams(),
-                'middleware_params' => $request->getMiddlewareParams()
-            ],
-            'response' => [
-                'status_code' => $response->getStatusCode(),
-                'body' => $response->getBody(),
-                'redirection' => $response->getRedirection() ?
-                    $response->getRedirection()['url']
-                    : null,
-                'execution_time' => number_format(microtime(true) - KX_START, 4),
-            ],
-        ];
+        try {
+            // log model class check
+            if (!class_exists('KX\Model\Logs')) {
 
-        $log = json_encode($log, JSON_PRETTY_PRINT) . ',';
+                $log = [
+                    'request' => [
+                        'date' => date('Y-m-d H:i:s'),
+                        'method' => $request->getRequestMethod(),
+                        'uri' => $request->getUri(),
+                        'query_string' => $request->getQueryString(),
+                        'header' => $request->getHeader(),
+                        'get_params' => $request->getGetParams(),
+                        'post_params' => $request->getPostParams(),
+                        'middleware_params' => $request->getMiddlewareParams()
+                    ],
+                    'response' => [
+                        'status_code' => $response->getStatusCode(),
+                        'body' => $response->getBody(),
+                        'redirection' => $response->getRedirection() ?
+                            $response->getRedirection()['url']
+                            : null,
+                        'execution_time' => number_format(microtime(true) - KX_START, 4),
+                    ],
+                ];
 
-        $logFile = KX_ROOT . 'app/Storage/logs/' . $response->getStatusCode() . '_' .
-            date('Ymd') . '_' . Helper::getIp() . '.log';
+                $log = json_encode($log, JSON_PRETTY_PRINT) . ',';
 
-        Helper::path('app/Storage/logs', true);
+                $logFile = KX_ROOT . 'app/Storage/logs/' . $response->getStatusCode() . '_' .
+                    date('Ymd') . '_' . Helper::getIp() . '.log';
 
-        if (!file_exists($logFile)) {
-            touch($logFile);
-        } else {
-            $log = PHP_EOL . $log;
+                Helper::path('app/Storage/logs', true);
+
+                if (!file_exists($logFile)) {
+                    touch($logFile);
+                } else {
+                    $log = PHP_EOL . $log;
+                }
+
+                file_put_contents($logFile, $log, FILE_APPEND);
+            } else {
+                $logModel = new \KX\Model\Logs();
+                $logModel->insert([
+                    'endpoint' => $request->getUri(),
+                    'status_code' => $response->getStatusCode(),
+                    'auth_token' => Helper::authToken(),
+                    'ip' => Helper::getIp(),
+                    'header' => Helper::getUserAgent(),
+                    'exec_time' => number_format(microtime(true) - KX_START, 4),
+                ]);
+            }
+        } catch (\Exception $e) {
+            // log error
+            $logFile = KX_ROOT . 'app/Storage/logs/' . 'error_' .
+                date('Ymd') . '_' . Helper::getIp() . '.log';
+
+            Helper::path('app/Storage/logs', true);
+
+            if (!file_exists($logFile)) {
+                touch($logFile);
+            }
+
+            file_put_contents($logFile, $e->getMessage() . PHP_EOL, FILE_APPEND);
         }
-
-        file_put_contents($logFile, $log, FILE_APPEND);
     }
 }
